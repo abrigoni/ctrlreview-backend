b7f892a9ae22418573d378c91c7525811564221c
abrigoni <brian.rigoni1@gmail.com>
2024-07-07 20:58:30 +0200

    cors localhost

--- ctrlreview_backend/settings.py
+++ ctrlreview_backend/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -62,11 +63,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
+    "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
 
